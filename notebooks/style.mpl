a994cf8133ed02f20718ce5d0fbcdd671f409650
Avi Flamholz <flamholz@gmail.com>
2025-05-16 09:40:23 -0400

    make the plotting code work

---
+++ notebooks/style.mpl
@@ -0,0 +1,20 @@
+axes.titlesize : 8
+axes.titlepad : 3
+axes.labelsize : 8
+axes.labelpad : 2
+axes.unicode_minus  : True
+lines.linewidth : 2
+lines.markersize : 6
+xtick.labelsize : 8
+ytick.labelsize : 8
+xtick.color : k
+xtick.labelcolor : k
+ytick.color : k
+ytick.labelcolor : k
+legend.fontsize : 7
+font.family : Lucida Sans OT
+font.size : 8
+axes.titlesize : 9
+mathtext.fontset : custom
+mathtext.rm : Lucida Sans OT
+mathtext.it : Lucida Sans OT:italic
--- notebooks/viz.py
+++ notebooks/viz.py
@@ -1,15 +1,12 @@
 import pandas as pd
 import squarify as sq
-#from bokeh.themes import Theme
-#from bokeh.models import *
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from matplotlib.path import Path
 from matplotlib.patches import BoxStyle
 from matplotlib.offsetbox import AnchoredText
 import seaborn as sns
-#import altair as alt
-#import bokeh.io
+
 
 
 def titlebox(
@@ -58,66 +55,6 @@ def ylabelbox(ax, text, color, bgcolor=None, size=6, boxsize=0.1, pad=0.05, **kw
     )
     cax.add_artist(at)
 
-def altair_theme():
-    colors = {'green': '#7AA974', 'light_green': '#BFD598',
-              'pale_green': '#DCECCB', 'yellow': '#EAC264',
-              'light_yellow': '#F3DAA9', 'pale_yellow': '#FFEDCE',
-              'blue': '#738FC1', 'light_blue': '#A9BFE3',
-              'pale_blue': '#C9D7EE', 'red': '#D56C55', 'light_red': '#E8B19D',
-              'pale_red': '#F1D4C9', 'purple': '#AB85AC',
-              'light_purple': '#D4C2D9', 'dark_green':'#7E9D90', 'dark_brown':'#905426'}
-    palette = [colors['red'], colors['blue'], colors['green'], 
-               colors['purple'], colors['dark_green'], colors['dark_brown'],
-               colors['yellow']]
-    
-    def _theme():
-        return {
-            'config': {
-                'background': 'white',
-                    'group': { 
-                    'fill': '#E3DCD0'
-                    },
-                'view': {
-                    'strokeWidth': 0,
-                    'height': 300,
-                    'width': 400,
-                    'fill': '#E3DCD0'
-                    },
-                'mark': {
-                    'strokeWidth': 0.5,
-                    'stroke': 'black'
-                },
-                'axis': {
-                    'domainColor': None,
-                    'labelFont': 'Lucida Sans',
-                    'titleFont': 'Lucida Sans',
-                    'titleFontWeight': 400,
-                    'grid': False,
-                    'ticks': True,
-                    'tickColor': 'white',
-                    'tickOffset': 8,
-                    'tickWidth': 1.5
-                },
-                'range': {
-                    'category': palette
-                },
-                'legend': {
-                    'labelFont': 'Lucida Sans',
-                    'titleFont': 'Lucida Sans',
-                    'titleFontWeight': 400
-                },
-                'title' : { 
-                    'font': 'Lucida Sans',
-                    'fontWeight': 400,
-                    'anchor': 'middle'
-
-                }
-                  }
-                }
-
-    alt.themes.register('pboc', _theme)# enable the newly registered theme
-    alt.themes.enable('pboc')
-
 def color_palette():
     """
     Returns a dictionary of the PBOC color palette
@@ -130,89 +67,13 @@ def color_palette():
               'pale_red': '#F1D4C9', 'purple': '#AB85AC',
               'light_purple': '#D4C2D9', 'dark_green':'#7E9D90', 'dark_brown':'#905426'}
 
-def bokeh_theme():
-    """A custom bokeh theme to match PBoC 2e colors"""
-    theme_json = {'attrs':
-            {'Figure': {
-                'background_fill_color': '#E3DCD0',
-                'outline_line_color': '#FFFFFF',
-            },
-            'Axis': {
-            'axis_line_color': "white",
-            'major_tick_in': 7,
-            'major_tick_line_width': 2.5,
-            'major_tick_line_color': "white",
-            'minor_tick_line_color': "white",
-            'axis_label_text_font': 'Helvetica',
-            'axis_label_text_font_style': 'normal'
-            },
-            'Grid': {
-                'grid_line_color': None,
-            },
-            'Legend': {
-                'background_fill_color': '#E3DCD0',
-                'border_line_color': '#FFFFFF',
-                'border_line_width': 1.5,
-                'background_fill_alpha': 0.5
-            },
-            'Text': {
-                'text_font_style': 'normal',
-               'text_font': 'Helvetica'
-            },
-            'Title': {
-                'background_fill_color': '#FFEDC0',
-                'text_font_style': 'normal',
-                'align': 'center',
-                'text_font': 'Helvetica',
-                'offset': 2,
-            }}}
-
-    theme = Theme(json=theme_json)
-    bokeh.io.curdoc().theme = theme
-
-    # Define the colors
-    colors = color_palette()
-    palette = [v for k, v in colors.items() if 'pale' not in k]
-    return [colors, palette]
-
-def load_js(fname, args):
-    """
-    Given external javascript file names and arguments, load a bokeh CustomJS
-    object
-    
-    Parameters
-    ----------
-    fname: str or list of str
-        The file name of the external javascript file. If the desired javascript
-        exists in multiple external files, they can be provided as a list of
-        strings.
-    args: dict
-        The arguments to supply to the custom JS callback. 
-    
-    Returns
-    -------
-    cb : bokeh CustomJS model object
-        Returns a bokeh CustomJS model object with the supplied code and
-        arguments. This can be directly assigned as callback functions.
-    """
-    if type(fname) == str:
-        with open(fname) as f:
-            js = f.read() 
-    elif type(fname) == list:
-        js = ''
-        for _fname in fname:
-            with open(_fname) as f:
-                js += f.read()
-
-    cb = CustomJS(code=js, args=args)
-    return cb
 
 def plotting_style(grid=False):
     """
     Sets the style to the publication style
     """
     rc = {'axes.facecolor': '#E3DCD0',
-          'font.family': 'Lucida Sans Unicode',
+          'font.family': 'Lucida Sans OT',
           'grid.linestyle': '-',
           'grid.linewidth': 0.5,
           'grid.alpha': 0.75,
@@ -232,7 +93,14 @@ def plotting_style(grid=False):
           'legend.facecolor': '#FFEDCE',
           'figure.dpi': 150,
            'xtick.color': 'k',
-           'ytick.color': 'k'}
+           'ytick.color': 'k',
+           # font sizes
+           'font.size': 8,
+           # title font size
+           'axes.titlesize': 8,
+           # label font size
+           'axes.labelsize': 8,
+           }
     plt.rc('text.latex', preamble=r'\usepackage{sfmath}')
     plt.rc('mathtext', fontset='stixsans', sf='sans')
     sns.set_style('darkgrid', rc=rc)
